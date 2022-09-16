c033892b2013eb965138a3f39ba2cb408f89050f
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-09-16 00:11:12 -0300

    Finally a descent looking progress bar with masking

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -5,7 +5,7 @@ public class ProgressBar : MonoBehaviour
 {
     private float maximum;
     private float current;
-    public Slider slider;
+    public Image mask;
 
     public float Current { set { current = value; } }
     public float Maximum { set { maximum = value; } }
@@ -18,6 +18,6 @@ public class ProgressBar : MonoBehaviour
     private void SetFillAmount()
     {
         float progress = current / maximum;
-        slider.value = progress;
+        mask.fillAmount = progress;
     }
 }
