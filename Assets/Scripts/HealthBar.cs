35ee6ed6fc2ff36820a2c7977f119b02095ec332
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-10-04 16:14:18 -0300

    Added text counter to HealthBar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
@@ -7,11 +8,25 @@ public class HealthBar : MonoBehaviour
     private float _maximum;
     [SerializeField]
     private float _current;
+    
     public Image mask;
+    public TextMeshProUGUI textLivesCounter;
 
     public float Current
     {
-        get { return _current; }
+        get
+        {
+            if (_current < Constants.HealthBar.Minimum)
+            {
+                return Constants.HealthBar.Minimum;
+            }
+            if (_current > _maximum)
+            {
+                return _maximum;
+            }
+
+            return _current;
+        }
         set
         {
             if (value < Constants.HealthBar.Minimum)
@@ -30,21 +45,29 @@ public class HealthBar : MonoBehaviour
     }
     public float Maximum { get { return _maximum; } set { _maximum = value; } }
 
+    private void SetFillAmount()
+    {
+        float progress = _current / _maximum;
+        mask.fillAmount = progress;
+    }
+
+    private void SetTextLivesCounter()
+    {
+        textLivesCounter.text = $"{Mathf.FloorToInt(Current)} / {Mathf.FloorToInt(Maximum)}";
+    }
+
     private void Start()
     {
         _maximum = Constants.HealthBar.DefaultMaximum;
         _current = _maximum;
+        SetFillAmount();
+        SetTextLivesCounter();
     }
 
     private void Update()
     {
         SetFillAmount();
-    }
-
-    private void SetFillAmount()
-    {
-        float progress = _current / _maximum;
-        mask.fillAmount = progress;
+        SetTextLivesCounter();
     }
 }
 
